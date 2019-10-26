53785ce952b7c8d090915620ec9b7c39c83e7141
Luis Falcon <luisdark935@hotmail.com>
2019-10-25 23:39:48 -0500

    Cambio de nombre de carpetas y establecimiento de gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 // Dependencias Gulp
-const gulp = require("gulp");
+const gulp = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass');
@@ -9,3 +9,16 @@ const autoprefixer = require('gulp-autoprefixer');
 const clean = require('gulp-clean');
 const zip = require('gulp-zip');
 const extReplace = require('gulp-ext-replace');
+
+gulp.task('css', () => {
+  return gulp.src('./landstorm/sass/core.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./landstorm/css/'))
+});
+
+gulp.task('js', () => {
+    return gulp.src('./landstorm/js/core.js')
+      .pipe(uglify())
+      .pipe(concat('core.min.js'))
+      .pipe(gulp.dest('./landstorm/js/'))
+});
